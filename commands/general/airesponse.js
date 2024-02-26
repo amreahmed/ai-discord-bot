7bfffc8782f666bb5fabc8a79f0eae3801f504ab
Amr Ezzat <amr.2023001640@buc.edu.eg>
2024-02-26 21:34:10 +0200

    bot

--- commands/general/airesponse.js
+++ commands/general/airesponse.js
@@ -1,26 +1,73 @@
 const request = require("request");
-const { ApplicationCommandOptionType } = require("discord.js");
+const {
+  ApplicationCommandOptionType,
+  EmbedBuilder,
+  ContextMenuCommandBuilder,
+  ApplicationCommandType,
+  REST,
+  Routes,
+} = require("discord.js");
+const puppeteer = require("puppeteer");
 const { CommandType } = require("wokcommands");
-const { EmbedBuilder } = require("discord.js");
 module.exports = {
-    name: "airesponse", // Required
-    category: "general", // Required for the slash commands
-    description: "get a response from ai", // Required for the slash commands
-    type: CommandType.SLASH, // If options are given then slash must be either true or 'both'
-    testOnly: false, // Ensure you have test servers setup
+  name: "airesponse", // Required
+  category: "general", // Required for the slash commands
+  description: "get a response from ai", // Required for the slash commands
+  type: CommandType.SLASH, // If options are given then slash must be either true or 'both'
+  testOnly: false,
+  deferReply: true, // Ensure you have test servers setup
 
-    options: [
-        {
-            name: "",
-            description: "Enter the song URL so the bot can download it.",
-            required: true,
-            type: ApplicationCommandOptionType.String,
-        },
-    ],
-    callback: ({ interaction, args, client }) => {
-    
+  options: [
+    {
+      name: "message",
+      description: "Enter the song URL so the bot can download it.",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    },
+  ],
+  callback: async ({ interaction, args, client, message }) => {
+     
       
+
+    async function getResponce() {
+      const browser = await puppeteer.launch({ headless: true });
+      const page = await browser.newPage();
+      await page.goto("https://chat-app-f2d296.zapier.app/");
+
+      const textBoxSelector = 'textarea[aria-label="chatbot-user-prompt"]';
+      await page.waitForSelector(textBoxSelector);
+
+      await page.type(textBoxSelector, args[0]);
+      await page.keyboard.press("Enter");
+
+      await page
+        .waitForSelector('[data-testid="final-bot-response"] p')
+        .catch((err) => {
+          return;
+        });
+
+        value = await page.$eval(
+          '[data-testid="final-bot-response"]',
+          (element) => element.textContent
+        );
+
+      await browser.close();
+
+        return value.trim();
+      }
       
+        const response = await getResponce();
+
+      const embed = new EmbedBuilder()
+          .setColor(0x0099ff)
+          .setTitle(`AI Response`)
+          .setDescription(response);
+      console.log(args[0]);
+
       
-    }
-}
+        interaction.editReply({ embeds: [embed] });
+      
+    },
+  
+
+};
--- commands/general/spotify.js
+++ commands/general/spotify.js
@@ -2,12 +2,14 @@ const request = require("request");
 const { ApplicationCommandOptionType } = require("discord.js");
 const { CommandType } = require("wokcommands");
 const { EmbedBuilder } = require("discord.js");
+const { defer } = require("request/lib/helpers");
 module.exports = {
   name: "spotify", // Required
   category: "general", // Required for the slash commands
   description: "download any song from spotify using the song link", // Required for the slash commands
   type: CommandType.SLASH, // If options are given then slash must be either true or 'both'
-  testOnly: false, // Ensure you have test servers setup
+  testOnly: false,
+  deferReply: true, // Ensure you have test servers setup
 
   options: [
     {
@@ -17,8 +19,7 @@ module.exports = {
       type: ApplicationCommandOptionType.String,
     },
   ],
-   callback: ({ interaction, args, client }) => {
-    
+  callback: ({ interaction, args, client }) => {
     const url = args[0];
 
     const options = {
@@ -36,50 +37,46 @@ module.exports = {
     request(options, function (error, response, body) {
       if (error) throw new Error(error);
 
-      
-        const parsedBody = JSON.parse(body);
+      const parsedBody = JSON.parse(body);
       console.log(parsedBody);
-      
-
-        const spotifyEmbed = new EmbedBuilder()
-          .setColor(0x0099ff)
-          .setTitle(`download ${parsedBody.title} from spotify`)
-          .setURL(parsedBody.download_link)
-          .setAuthor({
-            name: "CloxBot",
-          })
-          .setThumbnail(parsedBody.cover)
-          .addFields(
-            { name: "Type", value: `${parsedBody.type}`, inline: true },
-            {
-              name: "Artist",
-              value: `${parsedBody.artist}`,
-              inline: true,
-            }
-          )
-          .setTimestamp()
-          .setFooter({
-            text: "CloxBot",
-            iconURL: client.user.displayAvatarURL(),
-          });
+      const spotifyEmbed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle(`download ${parsedBody.title} from spotify`)
+        .setURL(parsedBody.download_link)
+        .setAuthor({
+          name: "CloxBot",
+        })
+        .setThumbnail(parsedBody.cover)
+        .addFields(
+          { name: "Type", value: `${parsedBody.type}`, inline: true },
+          {
+            name: "Artist",
+            value: `${parsedBody.artist}`,
+            inline: true,
+          }
+        )
+        .setTimestamp()
+        .setFooter({
+          text: "CloxBot",
+          iconURL: client.user.displayAvatarURL(),
+        });
 
       if (interaction) {
-          interaction.deferReply({ ephemeral: true });
-          if (parsedBody.message === "Invalid URL.") {
-            interaction.editReply({
-              content: "The URL you entered is invalid.",
-              ephemeral: true,
-            });
-          } else {
-              interaction.editReply({ embeds: [spotifyEmbed] });
-              console.log(parsedBody);
-            
-          }
+        if (parsedBody.message === "Invalid URL.") {
+          interaction.editReply({
+            content: "The URL you entered is invalid.",
+            ephemeral: true,
+          });
         } else {
-          // Handle non-interaction case if needed
+          interaction.editReply({
+            embeds: [spotifyEmbed],
+          });
           console.log(parsedBody);
         }
-
+      } else {
+        // Handle non-interaction case if needed
+        console.log(parsedBody);
+      }
     });
   },
 };
